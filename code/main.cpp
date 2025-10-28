08c1a7574492f21f65e50d275ba9271915f91443
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-10-28 00:45:08 -0700

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "ImageEditor-2.h"
 
+
 int main(){
-  ImageEditor editor("pic.png"); //REPLACE THIS WITH YOUR PNG FILE
+
+  ImageEditor editor("pic.png"); //REPLACE THIS WITH YOUR PNG FILE (h:327, w:280)
   -editor;                       //-editor is from Image editor class
   editor.save("output.png");     //After you run 'make run', the inverted image will be in 'output.png'
+
+  editor.asciiArt(1);			  //ASCII ART
+  editor.save("outputAscii.png"); //Grayscale image (ASCII will be shown in terminal)
+
+  editor.lineDetection(1);  //Line Detection with Sobel Operator
+  editor.save("lineDetectionFileName.png"); 
 
   return 0;
 }
